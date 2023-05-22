7a1c661f16f6e5007176cc6d3240da20a6c70149
Haj Mahdi (Mati) <dashmati4@gmail.com>
2023-05-22 14:51:58 +0200

    kunde og vare

---
+++ 2.1/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace _2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tb_cpr = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_adresse = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_tlf = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_savename = new System.Windows.Forms.Button();
+            this.tb_efternavn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_firstname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(928, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 29);
+            this.textBox1.TabIndex = 51;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(746, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 25);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Vare nr :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(928, 425);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(310, 29);
+            this.textBox2.TabIndex = 49;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(746, 429);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 25);
+            this.label8.TabIndex = 48;
+            this.label8.Text = "Pris :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(928, 370);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(310, 29);
+            this.textBox3.TabIndex = 47;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(746, 374);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 25);
+            this.label9.TabIndex = 46;
+            this.label9.Text = "Produkt navn :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(928, 317);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(310, 29);
+            this.textBox4.TabIndex = 45;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(746, 321);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 25);
+            this.label10.TabIndex = 44;
+            this.label10.Text = "Størrelse :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(928, 493);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 64);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "Gem vare";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(928, 263);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(310, 29);
+            this.textBox5.TabIndex = 42;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(746, 267);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 25);
+            this.label11.TabIndex = 41;
+            this.label11.Text = "TypeKode :";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(928, 211);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(310, 29);
+            this.textBox6.TabIndex = 40;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(746, 215);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(166, 25);
+            this.label12.TabIndex = 39;
+            this.label12.Text = "Produkt nummer :";
+            // 
+            // tb_cpr
+            // 
+            this.tb_cpr.Location = new System.Drawing.Point(303, 162);
+            this.tb_cpr.Name = "tb_cpr";
+            this.tb_cpr.Size = new System.Drawing.Size(310, 29);
+            this.tb_cpr.TabIndex = 38;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(187, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 25);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Cpr nr :";
+            // 
+            // tb_adresse
+            // 
+            this.tb_adresse.Location = new System.Drawing.Point(303, 429);
+            this.tb_adresse.Name = "tb_adresse";
+            this.tb_adresse.Size = new System.Drawing.Size(310, 29);
+            this.tb_adresse.TabIndex = 36;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(187, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 25);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Adresse :";
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(303, 374);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(310, 29);
+            this.tb_email.TabIndex = 34;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Email :";
+            // 
+            // tb_tlf
+            // 
+            this.tb_tlf.Location = new System.Drawing.Point(303, 321);
+            this.tb_tlf.Name = "tb_tlf";
+            this.tb_tlf.Size = new System.Drawing.Size(310, 29);
+            this.tb_tlf.TabIndex = 32;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Tlf nr :";
+            // 
+            // btn_savename
+            // 
+            this.btn_savename.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_savename.Location = new System.Drawing.Point(303, 497);
+            this.btn_savename.Name = "btn_savename";
+            this.btn_savename.Size = new System.Drawing.Size(310, 64);
+            this.btn_savename.TabIndex = 30;
+            this.btn_savename.Text = "Gem navn";
+            this.btn_savename.UseVisualStyleBackColor = false;
+            // 
+            // tb_efternavn
+            // 
+            this.tb_efternavn.Location = new System.Drawing.Point(303, 267);
+            this.tb_efternavn.Name = "tb_efternavn";
+            this.tb_efternavn.Size = new System.Drawing.Size(310, 29);
+            this.tb_efternavn.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Efternavn :";
+            // 
+            // tb_firstname
+            // 
+            this.tb_firstname.Location = new System.Drawing.Point(303, 215);
+            this.tb_firstname.Name = "tb_firstname";
+            this.tb_firstname.Size = new System.Drawing.Size(310, 29);
+            this.tb_firstname.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Fornavn :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 719);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tb_cpr);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_adresse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_tlf);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_savename);
+            this.Controls.Add(this.tb_efternavn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_firstname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tb_cpr;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_adresse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_tlf;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_savename;
+        private System.Windows.Forms.TextBox tb_efternavn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_firstname;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
